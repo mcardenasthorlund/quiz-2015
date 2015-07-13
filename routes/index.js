eb4b43f98569815360cdb83c53cf3ad24c2c7dde
mcardenasthorlund <mcardenasthorlund@users.noreply.github.com>
2015-07-13 13:05:52 +0200

    Primera pregunta
    Añadido el controlador de quiz y las vistas para la primera pregunta y
    respuesta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// Importamos el controlador de quiz
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+// Definimos las rutas
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
